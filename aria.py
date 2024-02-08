6914584a27b1294f6913a21e052a9c05af96788e
ArianneCielo <acielo23a-0134@olopsc.edu.ph>
2024-02-08 16:42:54 +0800

    Kayanin mo beh

--- aria.py
+++ aria.py
@@ -1,1 +1,11 @@
-print('hello world')
+students_scores = {
+	"Alice": [85,90,92],
+	"Bob": [78,80,85],
+	"Charlie": [92,88,95]}
+
+student_grades = [students_scores["Alice"], students_scores["Bob"], students_scores["Charlie"]]
+
+for student, grades in students_scores.items():
+	print(student)
+	for grade in grades:
+		print(f'{grade}')
